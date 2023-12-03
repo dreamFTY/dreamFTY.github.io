d8980ca45c762d062186c56a0358de56d1a5c909
test <email>
2023-12-03 08:25:03 +0800

    Site updated: 2023-12-03 08:25:02

--- js/copy.js
+++ js/copy.js
@@ -3,13 +3,13 @@ function addLink()
     var body_element = document.body; var selection;
     selection = window.getSelection(); 
     if (window.clipboardData) { // Internet Explorer 
-        var pagelink ="\r\n\r\n 原文出自[ Yi-Yun博客 ](yi-yun.github.io) \n除特别声明外，均采用CC BY-NC-SA 4.0许可协议\n转载请保留原文链接: "+document.location.href+""; 
+        var pagelink ="\r\n\r\n 原文出自[ Dream‘s bolg ](https://www.dreamfty.top) \n除特别声明外，均采用CC BY-NC-SA 4.0许可协议\n转载请保留原文链接: "+document.location.href+""; 
         var copytext = selection + pagelink;
         window.clipboardData.setData ("Text", copytext); 
         return false; 
     }
     else {
-        var pagelink = "<br/>原文出自[ Dream‘s bolg ](www.dreamfty.top)<br/>除特别声明外，均采用 CC BY-NC-SA 4.0 许可协议<br/>转载请保留原文链接: "+document.location.href+"";
+        var pagelink = "<br/>原文出自[ Dream‘s bolg ](https://www.dreamfty.top)<br/>除特别声明外，均采用 CC BY-NC-SA 4.0 许可协议<br/>转载请保留原文链接: "+document.location.href+"";
         var copytext = selection + "<br/>"+pagelink;
         var newdiv = document.createElement('div');
         newdiv.style.position='absolute';
